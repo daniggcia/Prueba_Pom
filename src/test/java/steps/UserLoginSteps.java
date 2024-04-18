f6fa7d2071ab171737b9c882a118a8a557a362ec
daniel <daniggcia@protonmail.com>
2024-04-18 13:23:09 +0200

    login button

--- src/test/java/steps/UserLoginSteps.java
+++ src/test/java/steps/UserLoginSteps.java
@@ -20,11 +20,6 @@ public class UserLoginSteps {
         loginPage = new LoginPage(driver);
     }
 
-    @And("^the user is on the login page$")
-    public void navigateToLoginPage() {
-        driver.get("https://www.saucedemo.com/");
-    }
-
 
     @When("^the user enters valid credentials$")
     public void enterValidCredentials() {
@@ -32,18 +27,24 @@ public class UserLoginSteps {
         loginPage.enterPassword("secret_sauce");
     }
 
+
+    @Then("^the user should be logged in successfully$")
+    public void verifyLoginSuccess() {
+        driver.get("https://www.saucedemo.com/inventory.html");
+    }
+
     @And("^the user clicks on the login button$")
     public void clickLoginButton() {
         loginPage.clickLoginButton();
     }
 
-    @Then("^the user should be logged in successfully$")
-    public void verifyLoginSuccess() {
-        // Agrega aquí las aserciones para verificar el inicio de sesión exitoso
+    @And("^the user is on the login page$")
+    public void navigateToLoginPage() {
+        driver.get("https://www.saucedemo.com/");
     }
 
-    @And("^the user close the browser$")
-    public void closeBrowser() {
+    @And("the user close the browser")
+    public void theUserCloseTheBrowser() {
         driver.quit();
     }
 }
